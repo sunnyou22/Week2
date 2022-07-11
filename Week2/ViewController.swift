b849121799750bc6d0071f5c3d482a3e29e8e880
sunnyou22 <sunnyouyaya22@gmail.com>
2022-07-11 23:08:59 +0900

    2주차 mon과제

--- Week2/ViewController.swift
+++ Week2/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cyanView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cyanView.layer.cornerRadius = 20
+//        cyanView.clipsToBounds = true
+        
+        cyanView.layer.maskedCorners = [.layerMaxXMinYCorner]
     }
 
-
+    @IBAction func slidertest(_ sender: UISlider) {
+        print("이렇게 프린트로 이벤트가 발생했는지 확인하는 것도 좋음")
+    }
+    
+    @IBAction func Byekeyboard(_ sender: UITextField) {
+    }
 }
 
